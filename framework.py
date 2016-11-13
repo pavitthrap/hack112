bc8c241fdb715980444f894734e9847faf0941a5
elxw <echo@Echos-Air.wv.cc.cmu.edu>
2016-11-13 13:28:10 -0500

    Framework with tracking!!!

--- framework.py
+++ framework.py
@@ -4,9 +4,17 @@ import os
 import csv
 import numpy as np
 import cv2
+import argparse
+import imutils
+from collections import deque 
 
 def rgbString(red, green, blue):
     return "#%02x%02x%02x" % (red, green, blue)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-b", "--buffer", type=int, default=64,
+    help="max buffer size")
+args = vars(ap.parse_args())
 
 def init(data):
     data.bgColor=rgbString(179, 204, 204)
@@ -14,6 +22,9 @@ def init(data):
     data.height= 800
     cap = cv2.VideoCapture(0)
     data.cap = cap
+    data.yellowLower = (20, 100, 100)
+    data.yellowUpper = (30, 255, 255)
+    data.pts = deque(maxlen=args["buffer"]) 
 
 def mousePressed(event, data):
     pass
@@ -43,14 +54,50 @@ def drawFrame(canvas, img, data): # draws each webcam frame
     canvas.create_image(xoffset,yoffset,anchor=NW,image=img)
 
 def getImage(data): # gets a new frame
-    ret,frame=data.cap.read()
+    (ret, frame) = data.cap.read()
     frame=cv2.flip(frame,1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    img=Image.fromarray(cv2image)
+
+    ############################
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+    mask = cv2.inRange(cv2image, data.yellowLower, data.yellowUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        if radius > 10:
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                       (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+    # update the points queue
+    data.pts.appendleft(center)
+
+    # loop over the set of tracked points
+    for i in range(1, len(data.pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if data.pts[i - 1] is None or data.pts[i] is None:
+            continue
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame, data.pts[i - 1], data.pts[i], (0, 0, 255), thickness)
+
+    finalFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img=Image.fromarray(finalFrame)
     h=675
     desiredW=900
     img=img.crop((0,0,desiredW,h))
     tkImg=ImageTk.PhotoImage(image=img)
+    print(data.pts)
     return tkImg
 
 def drawImage(canvas, width, height):
