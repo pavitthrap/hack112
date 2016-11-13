1d189eca555ce9df20f7e52e6f13b163a8e4baa4
Pavitthra Pandurangan <paviusa23@gmail.com>
2016-11-13 12:22:10 -0500

    g

---
+++ wikipedia-api-work.py
@@ -0,0 +1,41 @@
+import requests
+import json
+
+
+def main():
+    extract=getInput()
+    summary=parseExtract2(extract)
+    print(summary)
+
+def getInput():
+    j=-1
+    print("What article would you like to read?")
+    while j==-1:       
+        title=raw_input()
+        url='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exsentences=3&titles=%s&exsectionformat=plain&formatversion=2' % title
+        response = requests.get(url)
+        data=json.loads(response.content)
+        extract=data['query']['pages'][0]['extract']
+        if str(extract).find('refer') != -1:
+            j=0
+        print("Sorry, but the article you requested does not exist. Please suggest an alternate title.")
+
+    return extract
+
+
+
+def parseExtract2(extract):
+    extract=str(extract)
+    
+    j=extract.find('<')
+
+    while j!=-1:
+        endOne=extract.find('>')
+        extract=extract[:j]+extract[endOne+1:]
+        
+        j=extract.find('<')
+    return extract
+
+
+if __name__ == '__main__':
+  main()
--- wikipedia-api.py
+++ wikipedia-api.py
@@ -5,32 +5,38 @@ import json
 def main():
     #print("Which article do you want to try")
     #title=raw_input()
-    title='Gold'
     #url='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=short-summary&list=&meta=&titles=%s' % title
+    title=getInput()
     url='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exsentences=3&titles=%s&exsectionformat=plain&formatversion=2' % title
     response = requests.get(url)
     #data = response.json()
     data=json.loads(response.content)
-    extract=data['query']['pages'][0]['extract']
-    summary=parseExtract(extract)
+        extract=data['query']['pages'][0]['extract']
 
+        summary=parseExtract2(extract)
     #with open ('data3.json','w') as outfile:
      #   json.dump(data,outfile)
     #print(data)
 
-def parseExtract(extract):
+def getInput():
+    j=-1
+    while j==-1:
+        title=raw_input()
+        
+
+def parseExtract2(extract):
     extract=str(extract)
-    index=extract.find('<p><b>')
-    i=index+6
-    extract=extract[i:]
-    print(type(extract))
-    j=extract.find('</b>')
+    
+    j=extract.find('<')
     print(extract,j)
-    while j != -1:
-        extract=extract[:j]+extract[j+3:]
-        j=extract.find('<b>')
-        print(j)
-    print(extract)
+
+    while j!=-1:
+        endOne=extract.find('>')
+        extract=extract[:j]+extract[endOne+1:]
+        
+        j=extract.find('<')
+    return extract
+
 
 if __name__ == '__main__':
   main()
